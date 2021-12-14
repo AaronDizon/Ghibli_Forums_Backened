07117a461836e08ae65517c8c0d3df6244ed77c0
Aaron <imartindizon@gmail.com>
2021-12-14 11:23:51 -0800

    intialized backed

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const routesReport = require('rowdy-logger').begin(app)
+
+app.use(require('morgan')('tiny'))
+app.use(require('cors')())
+app.use(express.json())
+
+//routes
+
+
+app.listen(3001, () => {
+    routesReport.print()
+  })
