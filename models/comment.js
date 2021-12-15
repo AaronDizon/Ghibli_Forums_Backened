2833d473f32e3fa585db4c5cf4bbda8a9a5fe6c2
Aaron <imartindizon@gmail.com>
2021-12-15 09:50:12 -0800

    changed the column in comments from postId to threadId

--- models/comment.js
+++ models/comment.js
@@ -26,7 +26,7 @@ module.exports = (sequelize, DataTypes) => {
         notNull: true
       }
     },
-    postId: {
+    threadId: {
       type: DataTypes.INTEGER, allowNull: false, 
       validate: {
         notNull: true
